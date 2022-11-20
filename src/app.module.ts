8a5913f4afa5de32124c6f8b018c427397b526b7
WebertonMendes <weberton.mendes@hotmail.com>
2022-11-19 22:56:17 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,18 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      url: process.env.DATABASE_URL,
       type: 'postgres',
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
       entities: [process.env.TYPEORM_ENTITIES],
-      autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
     AuthModule,
     TasksModule,
